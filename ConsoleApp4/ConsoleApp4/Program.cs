2cf5106194269f9ec937e3d20aaeff9a89ea4f81
Glenhawk-0 <LennonEhare@Gmail.com>
2024-09-24 09:10:40 -0400

    le code , omg , so cool

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int myFavNum = 3;
+            int theBestNum = 4;
+
+            int sum;
+            sum = myFavNum + theBestNum;
+            Console.WriteLine(sum);
+
+            string prof = "...i litterally forgot... oops";
+            prof = "i remember now, its nick ";
+            Console.WriteLine(prof);
+        }
+    }
+}
